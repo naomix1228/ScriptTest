a19fab1ad6e5dedb757639ae79e1221a96e368a2
NAOMI IMAOKA <naomi_t50@yahoo.co.jp>
2016-09-27 15:23:05 +0900

    ★スクリプトファイルを二つに分けました。 通常課題（Test1)発展課題(Test2）です。
    ★発展課題は、大貫さんにお手伝いいただきました。

---
+++ Assets/Test2.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+
+    public int mp = 53;
+
+
+    public void Magic(int M)
+    {
+        if ( this.mp >= M)
+        {
+            this.mp -= M;
+            Debug.Log("魔法攻撃をした。残りmpは" + mp);
+        }
+
+        else
+        {
+            Debug.Log("MPが足りない！");
+        }
+        
+        
+    }
+}
+
+    
+
+
+public class Test2 : MonoBehaviour
+{
+    int Mpoint = 5;
+
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        
+        
+        
+     
+    for(int i = 0; i <11; i++)
+            {
+                lastboss.Magic(Mpoint);
+            }
+
+    
+       
+        }
+     
+            
+        
+    
+
+    void Update()
+    {
+
+    }
+}
+    
+        
+
+    
